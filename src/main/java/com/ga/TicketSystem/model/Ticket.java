8ca979fec89df1113e42c11cce7982fc0b4ffedd
Zahralsayed <zalmatooq31@gmail.com>
2026-03-30 19:37:08 +0300

    update Ticket model

--- src/main/java/com/ga/TicketSystem/model/Ticket.java
+++ src/main/java/com/ga/TicketSystem/model/Ticket.java
@@ -1,4 +1,21 @@
 package com.ga.TicketSystem.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String uniqueHash; // Digital signature/QR code data
+
+    @ManyToOne
+    @JoinColumn(name = "booking_id")
+    private Booking booking;
+
+    @ManyToOne
+    @JoinColumn(name = "seat_id")
+    private Seat seat;
 }
