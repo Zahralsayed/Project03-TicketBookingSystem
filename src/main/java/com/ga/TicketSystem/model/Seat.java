31a5258014d66f8bd6a79ca88654b27ffefc6b59
Zahralsayed <zalmatooq31@gmail.com>
2026-03-30 19:37:01 +0300

    update Seat model

--- src/main/java/com/ga/TicketSystem/model/Seat.java
+++ src/main/java/com/ga/TicketSystem/model/Seat.java
@@ -1,4 +1,24 @@
 package com.ga.TicketSystem.model;
 
+import com.ga.TicketSystem.enums.SeatStatus;
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name= "seats")
 public class Seat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String rowNumber;
+    private String seatNumber;
+    private SeatStatus status;
+
+    @Version
+    private Integer version; // Automatic Optimistic Locking
+
+    @ManyToOne
+    @JoinColumn(name = "location_id")
+    private Location location;
 }
