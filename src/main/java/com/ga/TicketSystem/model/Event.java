365e346c0de861b40afdc599c48693140db1792a
Zahralsayed <zalmatooq31@gmail.com>
2026-03-30 19:36:45 +0300

    update Event model

--- src/main/java/com/ga/TicketSystem/model/Event.java
+++ src/main/java/com/ga/TicketSystem/model/Event.java
@@ -1,4 +1,23 @@
 package com.ga.TicketSystem.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "events")
 public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String eventName;
+    private String description;
+    private LocalDateTime startTime;
+//    private String eventImageUrl; // Admin can upload event posters
+
+    @ManyToOne
+    @JoinColumn(name = "location_id")
+    private Location location;
 }
