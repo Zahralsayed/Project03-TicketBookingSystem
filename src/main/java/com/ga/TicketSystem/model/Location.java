fa0db9b8eaee19d4eb0a110113976080d01d9edc
Zahralsayed <zalmatooq31@gmail.com>
2026-03-30 19:36:51 +0300

    update Location model

--- src/main/java/com/ga/TicketSystem/model/Location.java
+++ src/main/java/com/ga/TicketSystem/model/Location.java
@@ -1,4 +1,26 @@
 package com.ga.TicketSystem.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.persistence.Table;
+
+import java.util.List;
+
+@Entity
+@Table(name = "locations")
 public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String address;
+    private int capacity;
+
+    @OneToMany(mappedBy = "location")
+    private List<Event> events;
+
+    @OneToMany(mappedBy = "location")
+    private List<Seat> seats;
 }
