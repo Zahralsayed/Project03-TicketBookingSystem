7e53f5c0b0d11f3f2fa0d2176bf64d8d39543884
Zahralsayed <zalmatooq31@gmail.com>
2026-03-30 19:36:39 +0300

    update Booking model

--- src/main/java/com/ga/TicketSystem/model/Booking.java
+++ src/main/java/com/ga/TicketSystem/model/Booking.java
@@ -1,4 +1,24 @@
 package com.ga.TicketSystem.model;
 
+
+import com.ga.TicketSystem.enums.BookingStatus;
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
 public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Double totalAmount;
+    private BookingStatus bookingStatus;
+
+    @Column(updatable = false)
+    private LocalDate createdAt;
+
+    @OneToMany(mappedBy = "booking", cascade = CascadeType.ALL)
+    private List<Ticket> tickets;
 }
