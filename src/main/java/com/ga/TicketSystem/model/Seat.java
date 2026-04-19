115a28de33db6aeae9071cb52bd1092aa99603f0
Zahralsayed <zalmatooq31@gmail.com>
2026-04-19 23:16:52 +0300

    seat category

--- src/main/java/com/ga/TicketSystem/model/Seat.java
+++ src/main/java/com/ga/TicketSystem/model/Seat.java
@@ -1,5 +1,7 @@
 package com.ga.TicketSystem.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.ga.TicketSystem.enums.SeatCategory;
 import com.ga.TicketSystem.enums.SeatStatus;
 import jakarta.persistence.Entity;
 import jakarta.persistence.*;
@@ -16,11 +18,13 @@ public class Seat {
     private String rowNumber;
     private String seatNumber;
     private SeatStatus status;
+    private SeatCategory category;
 
     @Version
     private Integer version; // Automatic Optimistic Locking
 
     @ManyToOne
     @JoinColumn(name = "location_id")
+    @JsonBackReference
     private Location location;
 }
