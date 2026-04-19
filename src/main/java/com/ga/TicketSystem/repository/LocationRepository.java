fec19f4b45c5744678bebccf53b13673adf88b94
Zahralsayed <zalmatooq31@gmail.com>
2026-04-19 23:20:59 +0300

    Update Location Repository

--- src/main/java/com/ga/TicketSystem/repository/LocationRepository.java
+++ src/main/java/com/ga/TicketSystem/repository/LocationRepository.java
@@ -1,7 +1,17 @@
 package com.ga.TicketSystem.repository;
 
+import com.ga.TicketSystem.dto.LocationSummaryDTO;
+import com.ga.TicketSystem.model.Event;
 import com.ga.TicketSystem.model.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 public interface LocationRepository extends JpaRepository<Location, Long> {
+    boolean existsByNameAndAddress(String name, String address);
+    Optional<Location> findByName(String name);
+    List<LocationSummaryDTO> findAllBy();
 }
