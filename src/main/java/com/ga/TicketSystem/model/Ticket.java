117d3bac5ab3cfde771e792ef282c145db8a9f18
Zahralsayed <zalmatooq31@gmail.com>
2026-04-20 19:03:08 +0300

    add constraint & price

--- src/main/java/com/ga/TicketSystem/model/Ticket.java
+++ src/main/java/com/ga/TicketSystem/model/Ticket.java
@@ -4,12 +4,15 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
-@Table(name = "tickets")
+@Table(name = "tickets", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"event_id", "seat_id"})})
 @Data
 public class Ticket {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private double price = 0.0;
 
     @Column(unique = true)
     private String uniqueHash; // Digital signature/QR code data
@@ -17,6 +20,10 @@ public class Ticket {
     @ManyToOne
     @JoinColumn(name = "booking_id")
     private Booking booking;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
 
     @ManyToOne
     @JoinColumn(name = "seat_id")
