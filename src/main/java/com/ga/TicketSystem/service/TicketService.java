229ef9f93ec646cef55325aa4534355e71e26a05
Zahralsayed <zalmatooq31@gmail.com>
2026-04-20 19:03:40 +0300

    calculate ticket price function

--- src/main/java/com/ga/TicketSystem/service/TicketService.java
+++ src/main/java/com/ga/TicketSystem/service/TicketService.java
@@ -1,7 +1,16 @@
 package com.ga.TicketSystem.service;
 
+import com.ga.TicketSystem.model.Event;
+import com.ga.TicketSystem.model.Seat;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TicketService {
+
+    public double calculateTicketPrice(Event event, Seat seat) {
+        Double base = event.getBasePrice();
+        Double multiplier = seat.getCategory().getMultiplier();
+
+        return base * multiplier;
+    }
 }
