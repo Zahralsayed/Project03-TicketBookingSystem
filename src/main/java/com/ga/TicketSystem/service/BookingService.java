72b96ac69ec829f2a3685a7af71c50fe6c2aa364
Zahralsayed <zalmatooq31@gmail.com>
2026-04-21 23:53:23 +0300

    sending email with booking confirmation Pdf after confirming the booking

--- src/main/java/com/ga/TicketSystem/service/BookingService.java
+++ src/main/java/com/ga/TicketSystem/service/BookingService.java
@@ -5,10 +5,15 @@ import com.ga.TicketSystem.dto.TicketDTO;
 import com.ga.TicketSystem.enums.BookingStatus;
 import com.ga.TicketSystem.model.*;
 import com.ga.TicketSystem.repository.*;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -36,91 +41,100 @@ public class BookingService {
     @Autowired
     private TicketService ticketService;
 
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Autowired
+    private BookingPDFService pdfService;
+
     private final ReentrantLock lock = new ReentrantLock();
 
-@Transactional
-public BookingResponseDTO createBooking(String email, Long eventId, List<Long> seatIds) {
-    lock.lock();
-    try {
-        System.out.println("Thread [" + Thread.currentThread().getName() + "] entered createBooking.");
-
-        Event event = eventRepository.findById(eventId)
-                .orElseThrow(() -> new EntityNotFoundException("Event with ID " + eventId + " not found."));
-
-        if (new HashSet<>(seatIds).size() != seatIds.size()) {
-            throw new RuntimeException("You cannot book the same seat twice in one order!");
-        }
-
-
-        for (Long seatId : seatIds) {
-            boolean isTaken = ticketRepository.existsByEventIdAndSeatIdAndBooking_BookingStatusNot(
-                    eventId, seatId, BookingStatus.CANCELED
-            );
-            if (isTaken) {
-                throw new RuntimeException("Seat " + seatId + " is already booked for this event.");
-            }
-        }
-
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new RuntimeException("User not found"));
-
-
-        Booking booking = new Booking();
-        booking.setUser(user);
-        booking.setBookingStatus(BookingStatus.PENDING);
-
-        double totalAmount = 0;
-        List<Ticket> tickets = new ArrayList<>();
-
-        for (Long seatId : seatIds) {
-            Seat seat = seatRepository.findById(seatId)
-                    .orElseThrow(() -> new RuntimeException("Seat " + seatId + " not found"));
-
-            Double ticketPrice = ticketService.calculateTicketPrice(event, seat);
-
-            Ticket ticket = new Ticket();
-            ticket.setBooking(booking);
-            ticket.setEvent(event);
-            ticket.setSeat(seat);
-            ticket.setPrice(ticketPrice);
-            ticket.setUniqueHash(UUID.randomUUID().toString());
-
-            tickets.add(ticket);
-            totalAmount += ticketPrice;
-        }
-
-        booking.setTickets(tickets);
-        booking.setTotalAmount(totalAmount);
-
+    @Transactional
+    public BookingResponseDTO createBooking(String email, Long eventId, List<Long> seatIds) {
+        lock.lock();
         try {
-            Booking savedBooking = bookingRepository.save(booking);
-            return convertToDTO(savedBooking);
-        } catch (DataIntegrityViolationException e) {
-            if (e.getRootCause().getMessage().contains("ukh31cugls")) {
-                throw new RuntimeException("Race condition: One or more selected seats were just booked.");
-            }
-            throw new RuntimeException("Booking failed: " + e.getRootCause().getMessage());
-        }
+            System.out.println("Thread [" + Thread.currentThread().getName() + "] entered createBooking.");
 
-    } finally {
-        lock.unlock();
-        System.out.println("Thread [" + Thread.currentThread().getName() + "] released the lock.");
+            Event event = eventRepository.findById(eventId)
+                    .orElseThrow(() -> new EntityNotFoundException("Event with ID " + eventId + " not found."));
+
+            if (new HashSet<>(seatIds).size() != seatIds.size()) {
+                throw new RuntimeException("You cannot book the same seat twice in one order!");
+            }
+
+
+            for (Long seatId : seatIds) {
+                boolean isTaken = ticketRepository.existsByEventIdAndSeatIdAndBooking_BookingStatusNot(
+                        eventId, seatId, BookingStatus.CANCELED
+                );
+                if (isTaken) {
+                    throw new RuntimeException("Seat " + seatId + " is already booked for this event.");
+                }
+            }
+
+            User user = userRepository.findByEmail(email)
+                    .orElseThrow(() -> new RuntimeException("User not found"));
+
+
+            Booking booking = new Booking();
+            booking.setUser(user);
+            booking.setBookingStatus(BookingStatus.PENDING);
+
+            double totalAmount = 0;
+            List<Ticket> tickets = new ArrayList<>();
+
+            for (Long seatId : seatIds) {
+                Seat seat = seatRepository.findById(seatId)
+                        .orElseThrow(() -> new RuntimeException("Seat " + seatId + " not found"));
+
+                Double ticketPrice = ticketService.calculateTicketPrice(event, seat);
+
+                Ticket ticket = new Ticket();
+                ticket.setBooking(booking);
+                ticket.setEvent(event);
+                ticket.setSeat(seat);
+                ticket.setPrice(ticketPrice);
+                ticket.setUniqueHash(UUID.randomUUID().toString());
+
+                tickets.add(ticket);
+                totalAmount += ticketPrice;
+            }
+
+            booking.setTickets(tickets);
+            booking.setTotalAmount(totalAmount);
+
+            try {
+                Booking savedBooking = bookingRepository.save(booking);
+                return convertToDTO(savedBooking);
+            } catch (DataIntegrityViolationException e) {
+                if (e.getRootCause().getMessage().contains("ukh31cugls")) {
+                    throw new RuntimeException("Race condition: One or more selected seats were just booked.");
+                }
+                throw new RuntimeException("Booking failed: " + e.getRootCause().getMessage());
+            }
+
+        } finally {
+            lock.unlock();
+            System.out.println("Thread [" + Thread.currentThread().getName() + "] released the lock.");
+        }
     }
-}
 
 
     @Transactional
-    public BookingResponseDTO confirmBooking(Long bookingId){
+    public BookingResponseDTO confirmBooking(Long bookingId) {
         Booking booking = bookingRepository.findById(bookingId)
                 .orElseThrow(() -> new EntityNotFoundException("Booking with ID " + bookingId + " not found."));
 
-        if (booking.getBookingStatus() != BookingStatus.PENDING){
-            throw new RuntimeException("Cannot confirm booking in "+ booking.getBookingStatus()+ " status.");
+        if (booking.getBookingStatus() != BookingStatus.PENDING) {
+            throw new RuntimeException("Cannot confirm booking in " + booking.getBookingStatus() + " status.");
         }
 
         booking.setBookingStatus(BookingStatus.CONFIRMED);
         Booking savedBooking = bookingRepository.save(booking);
 
+        byte[] pdfBytes = pdfService.generateBookingPdf(savedBooking);
+
+        sendEmailWithAttachment(savedBooking.getUser().getEmail(), "Your Booking Conformation", pdfBytes, savedBooking.getId());
         return convertToDTO(savedBooking);
     }
 
@@ -153,7 +167,6 @@ public BookingResponseDTO createBooking(String email, Long eventId, List<Long> s
     }
 
 
-
     private BookingResponseDTO convertToDTO(Booking booking) {
         BookingResponseDTO dto = new BookingResponseDTO();
         dto.setBookingId(booking.getId());
@@ -176,5 +189,23 @@ public BookingResponseDTO createBooking(String email, Long eventId, List<Long> s
         return dto;
     }
 
+    private void sendEmailWithAttachment(String to, String subject, byte[] pdfBytes, Long bookingId) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText("Hello! Thank you for your booking. Please find your tickets attached below.");
+
+            helper.addAttachment("Booking_Ticket_" + bookingId + ".pdf", new ByteArrayResource(pdfBytes));
+
+            mailSender.send(message);
+        } catch (MessagingException e) {
+
+            System.err.println("Failed to send email: " + e.getMessage());
+        }
+
+
+    }
 }
