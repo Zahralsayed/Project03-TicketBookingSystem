f72ad91222ba70af6e0cef3ac6faa83a7ae3db4a
Zahralsayed <zalmatooq31@gmail.com>
2026-04-21 19:54:48 +0300

    delete, cancel, confirm logic

--- src/main/java/com/ga/TicketSystem/service/BookingService.java
+++ src/main/java/com/ga/TicketSystem/service/BookingService.java
@@ -109,7 +109,48 @@ public BookingResponseDTO createBooking(String email, Long eventId, List<Long> s
 }
 
 
+    @Transactional
+    public BookingResponseDTO confirmBooking(Long bookingId){
+        Booking booking = bookingRepository.findById(bookingId)
+                .orElseThrow(() -> new EntityNotFoundException("Booking with ID " + bookingId + " not found."));
 
+        if (booking.getBookingStatus() != BookingStatus.PENDING){
+            throw new RuntimeException("Cannot confirm booking in "+ booking.getBookingStatus()+ " status.");
+        }
+
+        booking.setBookingStatus(BookingStatus.CONFIRMED);
+        Booking savedBooking = bookingRepository.save(booking);
+
+        return convertToDTO(savedBooking);
+    }
+
+    @Transactional
+    public void cancelBooking(Long bookingId, String userEmail) {
+        Booking booking = bookingRepository.findById(bookingId)
+                .orElseThrow(() -> new EntityNotFoundException("Booking not found with ID: " + bookingId));
+
+        if (!booking.getUser().getEmail().equals(userEmail)) {
+            throw new RuntimeException("You are not authorized to cancel this booking.");
+        }
+
+        if (booking.getBookingStatus() == BookingStatus.CANCELED) {
+            throw new RuntimeException("Booking is already canceled.");
+        } else if (booking.getBookingStatus() == BookingStatus.CONFIRMED) {
+            throw new RuntimeException("Your booking is confirmed, You Cannot cancel this booking.");
+        }
+
+        booking.setBookingStatus(BookingStatus.CANCELED);
+
+        bookingRepository.save(booking);
+    }
+
+    @Transactional
+    public void deleteBooking(Long bookingId) {
+        if (!bookingRepository.existsById(bookingId)) {
+            throw new EntityNotFoundException("Booking not found.");
+        }
+        bookingRepository.deleteById(bookingId);
+    }
 
 
 
